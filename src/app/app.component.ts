f32a7f29e0b99b3dfe187bbfddbfb1e16fdabc68
trajil <trajil@web.de>
2025-03-13 11:12:19 +0100

    fix: deleted routeroutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HashingComponent } from './hashing/hashing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HashingComponent],
+  imports: [HashingComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
